bef382fd4f993d3b8a153864d0c2cd5303e60817
Thanhaz124 <thanhhaichau2002@gmail.com>
2021-09-18 18:46:34 +0700

    luKHolandau

---
+++ minhhoabien/minhhoabien.java
@@ -0,0 +1,10 @@
+public class minhhoabien {
+    public static void main(String[] args) {
+        float diemLTHuongDoiTuong=0.5F;
+        double diemKTLapTrinh;
+        //ghi
+        diemKTLapTrinh=10.0;
+        System.out.println("diem mon ki thuat lap trinh:"+diemKTLapTrinh);
+    }
+    
+}
